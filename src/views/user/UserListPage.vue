e478150609c1dc75f06012481a2fe11c9f230d96
Park JeongSu <pjs91_marrow@naver.com>
2025-10-15 12:32:32 +0900

    다국어 설정

--- src/views/user/UserListPage.vue
+++ src/views/user/UserListPage.vue
@@ -28,52 +28,52 @@ import { ref } from 'vue'
 
 // 검색 및 폼 스키마 정의
 const searchSchema = ref([
-  { key: 'userId', labelKey: 'tableHeaders.userId', component: 'v-text-field' },
-  { key: 'productName', labelKey: '제품명', component: 'v-text-field' },
-  {
-    key: 'status',
-    labelKey: '상태',
-    component: 'v-select',
-    items: ['판매중', '품절', '단종'],
-  },
+  { key: 'userId', labelKey: 'columns.userId', component: 'v-text-field' },
+  { key: 'authorityId', labelKey: 'columns.authorityId', component: 'v-text-field' },
+  { key: 'userName', labelKey: 'columns.userName', component: 'v-text-field' },
+  { key: 'email', labelKey: 'columns.email', component: 'v-text-field' },
+  { key: 'phone1', labelKey: 'columns.phone1', component: 'v-text-field' },
 ])
 
 // 폼 스키마 정의 (추가/수정에 사용)
 const formSchema = ref([
-  { key: 'userId', labelKey: 'tableHeaders.userId', component: 'v-text-field' },
-  { key: 'productName', labelKey: '제품명', component: 'v-text-field' },
-  {
-    key: 'status',
-    labelKey: '상태',
+  { key: 'userId', labelKey: 'columns.userId', component: 'v-text-field' },
+  { key: 'authorityId', labelKey: 'columns.authorityId', component: 'v-select', items: [] },
+  { key: 'userName', labelKey: 'columns.userName', component: 'v-text-field' },
+  { key: 'password', labelKey: 'columns.password', component: 'v-text-field' },
+  { key: 'email', labelKey: 'columns.email', component: 'v-text-field' },
+  { key: 'phone1', labelKey: 'columns.phone1', component: 'v-text-field' },
+  { key: 'phone2', labelKey: 'columns.phone2', component: 'v-text-field' },
+])
+
+/**
+ *   {
+    key: 'userName',
+    labelKey: 'userName',
     component: 'v-select',
     items: ['판매중', '품절', '단종'],
   },
-  {
-    key: 'price',
-    labelKey: '가격',
-    component: 'v-text-field',
-    type: 'number',
-  },
-])
+ *
+ */
 
 const headers = ref([
-  //{ title: 'tableHeaders.id', key: 'id' },
-  { title: 'tableHeaders.userId', key: 'userId' },
-  { title: 'tableHeaders.authorityId', key: 'authorityId' },
-  { title: 'tableHeaders.userName', key: 'userName' },
-  { title: 'tableHeaders.password', key: 'password' },
-  { title: 'tableHeaders.email', key: 'email' },
-  { title: 'tableHeaders.phone1', key: 'phone1' },
-  { title: 'tableHeaders.phone2', key: 'phone2' },
-  { title: 'tableHeaders.checkOutState', key: 'checkOutState' },
-  { title: 'tableHeaders.checkOutTime', key: 'checkOutTime' },
-  { title: 'tableHeaders.checkOutUser', key: 'checkOutUser' },
-  { title: 'tableHeaders.dataState', key: 'dataState' },
-  { title: 'tableHeaders.eventName', key: 'eventName' },
-  { title: 'tableHeaders.timeKey', key: 'timeKey' },
-  { title: 'tableHeaders.eventTime', key: 'eventTime' },
-  { title: 'tableHeaders.eventUser', key: 'eventUser' },
-  { title: 'tableHeaders.eventComment', key: 'eventComment' },
+  //{ title: 'columns.id', key: 'id' },
+  { title: 'columns.userId', key: 'userId' },
+  { title: 'columns.authorityId', key: 'authorityId' },
+  { title: 'columns.userName', key: 'userName' },
+  { title: 'columns.password', key: 'password' },
+  { title: 'columns.email', key: 'email' },
+  { title: 'columns.phone1', key: 'phone1' },
+  { title: 'columns.phone2', key: 'phone2' },
+  { title: 'columns.checkOutState', key: 'checkOutState' },
+  { title: 'columns.checkOutTime', key: 'checkOutTime' },
+  { title: 'columns.checkOutUser', key: 'checkOutUser' },
+  { title: 'columns.dataState', key: 'dataState' },
+  { title: 'columns.eventName', key: 'eventName' },
+  { title: 'columns.timeKey', key: 'timeKey' },
+  { title: 'columns.eventTime', key: 'eventTime' },
+  { title: 'columns.eventUser', key: 'eventUser' },
+  { title: 'columns.eventComment', key: 'eventComment' },
 ])
 </script>
 <style scoped></style>
